29f52b7bbb8e51361785a2f8ee04456144f26f0b
Jess Latimer <jplatimer@gmail.com>
2014-07-15 12:05:57 -0700

    removed unused files from package.s

--- package.js
+++ package.js
@@ -7,13 +7,13 @@ Package.on_use(function (api) {
 
   api.add_files([
     'urlify2.js',
-    'n11n.js',
+    // 'n11n.js',
     'urlify.js',
     'urlify-more.js'
   ], ['client', 'server']);
 });
 
-Package.on_test(function (api) {
-  api.use(['urlify2', 'tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('tests.js', ['client', 'server']);
-});
+// Package.on_test(function (api) {
+//   api.use(['urlify2', 'tinytest', 'test-helpers'], ['client', 'server']);
+//   api.add_files('tests.js', ['client', 'server']);
+// });
